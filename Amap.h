5278aea9a0255deba9892be036f413e126c4a965
Christian Gaser <christian.gaser@uni-jena.de>
2009-01-12 23:03:58 +0000

    Used defined names for PVE method.
    git-svn-id: svn://141.35.69.214/amap@31 3806922a-3115-4c2f-bdad-02e90158041f

--- Amap.h
+++ Amap.h
@@ -4,12 +4,27 @@
  *
  */
 
+#define SQRT2PI 2.506628
+#define G 6
+
+#define MAX_NC 5
+#define TH_COLOR 1
+#define TH_CHANGE 0.00001
+#define TINY 1e-15 
+
+#define NOPVE 0
+#define MARGINALIZED 1
+#define KMEANS 2
+
+#define CSFLABEL   0
+#define GMCSFLABEL 1
+#define GMLABEL    2
+#define WMGMLABEL  3
+#define WMLABEL    4
+
 #ifndef SQR
 #define SQR(x) ((x)*(x))
 #endif
-
-#define SQRT2PI 2.506628
-#define G 6
 
 #ifndef MAX
 #define MAX(A,B) ((A) > (B) ? (A) : (B))
@@ -18,17 +33,6 @@
 #ifndef MIN
 #define MIN(A,B) ((A) < (B) ? (A) : (B))
 #endif
-
-#define MAX_NC 5
-#define TH_COLOR 1
-#define TH_CHANGE 0.00001
-#define TINY 1e-15 
-
-#define CSFLABEL   0
-#define GMCSFLABEL 1
-#define GMLABEL    2
-#define WMGMLABEL  3
-#define WMLABEL    4
 
 #ifndef ROUND
 #define ROUND( x ) ((int) ((x) + ( ((x) >= 0) ? 0.5 : (-0.5) ) ))
