b5f54096b5b517c18aa8fb5b43aae072dcd506a5
Christian Gaser <christian.gaser@uni-jena.de>
2010-01-19 07:47:31 +0000

    Added ORNLM as niiornlm.c.
    git-svn-id: svn://141.35.69.214/amap@119 3806922a-3115-4c2f-bdad-02e90158041f

---
+++ niiornlm.c
@@ -0,0 +1,72 @@
+/*
+ * Christian Gaser
+ * $Id: niismooth.c 95 2009-11-05 16:02:56Z gaser $ 
+ *
+ */
+
+#include <ParseArgv.h>
+#include <float.h>
+#include <stdlib.h>
+
+#include "nifti1/nifti1_io.h"
+#include "nifti1/nifti1_local.h"
+
+extern nifti_image *read_nifti_float( const char *input_filename, double *image[]);
+extern write_nifti( const char *output_filename, double image[], int data_type, double slope, int dim[], double vox[], nifti_image *in_ptr);
+
+double h;
+
+static ArgvInfo argTable[] = {
+  {"-h", ARGV_FLOAT, (char *) 1, (char *) &h, 
+       "FWHM in mm."},
+   {NULL, ARGV_END, NULL, NULL, NULL}
+};
+
+/* Main program */
+
+int main(int argc, char *argv[])
+{
+  char *infile, *outfile;
+  int i, j, dims[3];
+  double *input, *output, separations[3];
+  nifti_image *nii_ptr;
+
+  /* Get arguments */
+  if  (ParseArgv(&argc, argv, argTable, 0) ||(argc < 2)) {
+   (void) fprintf(stderr, 
+   "\nUsage: %s -h smoothing_size <in.nii> <out.nii>\n",
+        argv[0]);
+   (void) fprintf(stderr, 
+   "    %s -help\n\n", argv[0]);
+   exit(EXIT_FAILURE);
+  }
+  
+  infile  = argv[1];
+  outfile = argv[2];
+  
+  /* read first image to get image parameters */
+  nii_ptr = read_nifti_float(infile, &input);
+  if(nii_ptr == NULL) {
+    fprintf(stderr,"Error reading %s.\n", infile);
+    return(EXIT_FAILURE);
+  }
+
+  separations[0] = nii_ptr->dx;
+  separations[1] = nii_ptr->dy;
+  separations[2] = nii_ptr->dz;
+  dims[0] = nii_ptr->nx;
+  dims[1] = nii_ptr->ny;
+  dims[2] = nii_ptr->nz;
+  
+  output = (double *)malloc(sizeof(double)*dims[0]*dims[1]*dims[2]);
+  
+  ornlm(input, output, 3, 1, h, dims);
+
+  if (!write_nifti( outfile, output, DT_FLOAT32, 1.0, dims, separations, nii_ptr)) 
+    exit(EXIT_FAILURE);
+
+  free(input);
+  
+  return(EXIT_SUCCESS);
+
+}
