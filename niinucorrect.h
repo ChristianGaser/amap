4d1e53537c6f251a6db84d43176dc62708d4c229
Christian Gaser <christian.gaser@uni-jena.de>
2011-02-16 21:13:50 +0000

    Changed paths: A      niinucorrect.h
    Added niinucorrect.h.
    
    
    
    
    git-svn-id: svn://141.35.69.214/amap@149 3806922a-3115-4c2f-bdad-02e90158041f

---
+++ niinucorrect.h
@@ -0,0 +1,25 @@
+/*
+ * Christian Gaser
+ * $Id: niiamap.h 128 2010-03-15 10:50:02Z gaser $ 
+ *
+ */
+
+char *mask_filename = NULL;
+int n_pure_classes = 3;
+int iters_amap = 200;
+int subsample = 16;
+int iters_nu = 50;
+int iters_ICM = 50;
+int pve = 5;
+int correct_nu = 1;
+int write_seg[3] = {0, 1, 0};
+int write_nu = 0;
+int write_label = 1;
+#ifdef SPLINESMOOTH
+  double bias_fwhm = 500.0;
+#else
+  double bias_fwhm = 60.0;
+#endif
+double thresh_brainmask = 0.05;
+double thresh_kmeans = 0.5;
+double weight_MRF = 0.15;
